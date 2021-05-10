25967c16c609c5d72da23ecd3ad80f0c465190cf
郑圳宏 <1131177344@qq.com>
2021-05-10 11:40:19 +0800

    chat1

---
+++ src/service/base.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+const ERR_OK = 0
+const baseURL = '/'
+axios.defaults.baseURL = baseURL
+
+export function get(url, params) {
+  return axios.get(url, { params }).then((res) => {
+    const serveData = res.data
+    if (serveData.code === ERR_OK) {
+      return serveData.result
+    }
+  }).catch((e) => {
+    console.log(e)
+  })
+}
---
+++ src/views/top-list.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>排行</div>
+</template>
+
+<script >
+export default {
+  name: 'top-list'
+}
+</script>
